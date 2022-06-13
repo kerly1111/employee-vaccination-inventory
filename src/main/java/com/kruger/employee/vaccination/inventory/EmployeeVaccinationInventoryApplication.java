33164ab08ea642880778d31fb4a8e4824217b3f6
Kerly Martinez <krmc_94@hotmail.com>
2022-06-12 19:12:55 -0500

    Project Initialization
    Project Initialization

---
+++ src/main/java/com/kruger/employee/vaccination/inventory/EmployeeVaccinationInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.employee.vaccination.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeVaccinationInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeVaccinationInventoryApplication.class, args);
+	}
+
+}
